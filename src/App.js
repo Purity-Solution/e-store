d4ff685a10c9ece378db0c53f3f19b40ec12111c
Muhammad Waseem <muhammadwaseem2u@gmail.com>
2022-11-08 09:31:25 -0800

    register-ui

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Products from "./components/Products/Products";
 import Login from "./components/Login/Login";
 import Cart from "./components/cart/Cart";
 import Register from "./components/Register/Register";
+import Undifined from "./components/undifinedPage/Undifined";
 function App() {
   return (
     <div>
@@ -19,6 +20,7 @@ function App() {
         <Route path="/log" element={<Login />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/reg" element={<Register />} />
+        <Route path="/undifined" element={<Undifined />} />
       </Routes>
     </div>
   );
---
+++ src/components/Button/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./button.scss";
+const Button = () => {
+  return (
+    <div className="col-sm-12 login">
+      <button className="button">Login</button>
+    </div>
+  );
+};
+
+export default Button;
