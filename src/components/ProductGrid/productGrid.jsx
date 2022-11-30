d0da54e4a8173dba0244fea4c36158467767489c
Muhammad Waseem <muhammadwaseem2u@gmail.com>
2022-11-30 00:33:09 -0800

    add product item

--- src/components/ProductGrid/productGrid.jsx
+++ src/components/ProductGrid/productGrid.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./productGrid.scss";
 import { lists } from "./productGridApi";
 import { listCategory } from "./productListApi";
+import { option } from "./productOptionApi";
+import { item } from "./productItemApi";
 const ProductGrid = () => {
   return (
     <div>
@@ -21,6 +24,47 @@ const ProductGrid = () => {
               </select>
             </div>
           </div>
+          <div className="col-12 col-sm-4">
+            <div className="option">
+              <span>0 Items</span>
+              <select className="selectOption">
+                {option.map((x) => (
+                  <option className="listNo">{x.option}</option>
+                ))}
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          {item.map((x) => (
+            <div className="col-12 col-sm-4">
+              <div className="gridItem">
+                <Link to="/" className="productLink">
+                  <div className="imageWrap">
+                    <div
+                      className="overly"
+                      style={{ backgroundImage: `url(${x.wave})` }}
+                    ></div>
+                    <Link className="title">
+                      <img src={x.image} alt="brush" className="wrap" />
+                    </Link>
+                  </div>
+                  <div className="discription">
+                    <h6 className="pricevariation">{x.price}</h6>
+                    <div className="productvariation">
+                      <h6 className="pricevariation">
+                        <span className="startPrice">{x.line}</span>
+                        <span className="currency">{x.curr}</span>
+                        "15.75"
+                      </h6>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
---
+++ src/components/ProductGrid/productItemApi.js
@@ -0,0 +1,37 @@
+export const item = [
+  {
+    wave: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAfQA…feYBBYxSMODAa6SMQAADaMw0aB/4n/gAAAABJRU5ErkJggg==",
+    image: "https://pigeon.b-cdn.net/album3%2FFeatured%20image.jpeg",
+    price: "Training Toothbrush",
+    line: "Starting From",
+    curr: "AED",
+  },
+  {
+    wave: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAfQA…feYBBYxSMODAa6SMQAADaMw0aB/4n/gAAAABJRU5ErkJggg==",
+    image: "https://pigeon.b-cdn.net/album3%2FFeatured%20image.jpeg",
+    price: "Training Toothbrush",
+    line: "Starting From",
+    curr: "AED",
+  },
+  {
+    wave: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAfQA…feYBBYxSMODAa6SMQAADaMw0aB/4n/gAAAABJRU5ErkJggg==",
+    image: "https://pigeon.b-cdn.net/album3%2FFeatured%20image.jpeg",
+    price: "Training Toothbrush",
+    line: "Starting From",
+    curr: "AED",
+  },
+  {
+    wave: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAfQA…feYBBYxSMODAa6SMQAADaMw0aB/4n/gAAAABJRU5ErkJggg==",
+    image: "https://pigeon.b-cdn.net/album3%2FFeatured%20image.jpeg",
+    price: "Training Toothbrush",
+    line: "Starting From",
+    curr: "AED",
+  },
+  {
+    wave: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAfQA…feYBBYxSMODAa6SMQAADaMw0aB/4n/gAAAABJRU5ErkJggg==",
+    image: "https://pigeon.b-cdn.net/album3%2FFeatured%20image.jpeg",
+    price: "Training Toothbrush",
+    line: "Starting From",
+    curr: "AED",
+  },
+];
