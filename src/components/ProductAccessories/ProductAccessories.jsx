afa9c4dbc88d7c5a9ec22086d532d5f3ddf2802c
Muhammad Waseem <muhammadwaseem2u@gmail.com>
2022-12-06 23:45:12 -0800

    created ProductAccessories component

---
+++ src/components/ProductAccessories/ProductAccessories.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./productAccessories.scss";
+
+const ProductAccessories = () => {
+  return (
+    <div>
+      <h1>ProductAccessories</h1>
+    </div>
+  );
+};
+
+export default ProductAccessories;
